af4a600dc6d540a3fee276d8c2df956a8e823d94
mhimranhossain <mh.imranhossain@outlook.com>
2023-04-24 09:30:57 +0600

    Update 04/24/2023 09:30:57

--- src/routes/imagegallery/+page.svelte
+++ src/routes/imagegallery/+page.svelte
@@ -1,20 +1,22 @@
-<script>
+<script lang="ts">
   import HomeLink from "$lib/HomeLink.svelte"
-  import img1 from "$lib/assets/imgs/1.jpg"
-  import img2 from "$lib/assets/imgs/2.jpg"
-  import img3 from "$lib/assets/imgs/3.jpg"
-  import img4 from "$lib/assets/imgs/4.jpg"
-  import img5 from "$lib/assets/imgs/5.jpg"
-  import img6 from "$lib/assets/imgs/6.jpg"
+  import { onMount } from "svelte"
 
+  let images: string[] = []
 
-  let list = [img1, img2, img3, img4, img5, img6]
+  onMount(async () => {
+    const imageFiles = import.meta.glob("../../imgs/*.{jpg,png,gif}")
+    for (const path in imageFiles) {
+      const image: any = await imageFiles[path]()
+      images = [...images, image.default]
+    }
+  })
 </script>
 
 <HomeLink />
 <div class="flex justify-center items-center min-h-screen">
   <div class="p-4 grid gap-4 sm:grid-cols-2 xl:grid-cols-3">
-    {#each list as img}
+    {#each images as img}
       <img src={img} alt="" class="rounded-lg" />
     {/each}
   </div>
